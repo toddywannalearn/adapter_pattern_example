9d144646d7dd7b54255c9622b8c52d47cecfc428
Douglas <toddynotes@gmail.com>
2022-11-29 00:06:48 -0300

    Feat: Initial Commit

---
+++ bin/adapter_pattern.dart
@@ -0,0 +1,5 @@
+import 'package:adapter_pattern/adapter_pattern.dart' as adapter_pattern;
+
+void main(List<String> arguments) {
+  print('Hello world: ${adapter_pattern.calculate()}!');
+}
---
+++ test/adapter_pattern_test.dart
@@ -0,0 +1,8 @@
+import 'package:adapter_pattern/adapter_pattern.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
